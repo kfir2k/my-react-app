3b3897597763858f632ac11bc55ebe5d151b74e0
Kfir Kotler <kfir.kotler@gmail.com>
2024-02-25 19:25:10 +0200

    Commit

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import './home.css';
+import React, { Component } from 'react'
+
+class Home extends Component {
+	state = {  } 
+	render() { 
+		return (
+			<div className="home">
+				This is the home Component!
+			</div>
+		);
+	}
+}
+ 
+export default Home;
---
+++ src/components/PageHeader.js
@@ -0,0 +1,11 @@
+const PageHeader = () => {
+	return ( 
+
+		<div class="center">
+			<h1>PageHeader</h1>
+		</div>
+
+	 );
+}
+ 
+export default PageHeader;
